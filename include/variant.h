212768c7b21815ecb5342e78206ed8dea21239ec
Navilks <joryvardas@gmail.com>
2020-10-25 16:47:11 -0600

    Added variant_join function to construct an std::variant out of multiple std::variants.  This will be useful for constructing std::variant types in a more modular way. Added holds_any_alternative to all checking if an std::variant is currently holding any of the alternatives, rather than just one.  This uses variant_join on the list of alternatives, which means that in order to check if the std::variant holds an std::variant then you would have to write something like holds_any_alternative<std::variant<std::variant<_some_types_>>(_variant_to_check_).

---
+++ include/variant.h
@@ -0,0 +1,55 @@
+#include <variant>
+#include "typeTraits.h"
+
+#ifndef VARIANT_H
+#define VARIANT_H
+
+template<typename ...Ty>
+struct _variant_join_helper;
+template<typename ...A, typename ...B>
+struct _variant_join_helper<std::variant<A...>, std::variant<B...>>{
+    using _type = std::variant<A..., B...>;
+};
+
+template <typename ...Ty>
+struct _variant_join;
+template <typename NonVariantType>
+struct _variant_join<NonVariantType>{
+    using _type = std::variant<NonVariantType>;
+};
+template <typename ...VariantTypes>
+struct _variant_join<std::variant<VariantTypes...>>{
+    using _type = std::variant<VariantTypes...>;
+};
+template <typename FirstVariant, typename ...RemainingVariants>
+struct _variant_join<FirstVariant, RemainingVariants...>{
+    using _firstVariantType = typename _variant_join<FirstVariant>::_type;
+    using _remainingVariantType = typename _variant_join<RemainingVariants...>::_type;
+    using _type = typename _variant_join_helper<_firstVariantType, _remainingVariantType>::_type;
+};
+
+template<typename ...Variants>
+using variant_join = typename _variant_join<Variants...>::_type;
+
+
+
+
+template<typename ...Ty>
+struct _holds_any_alternative_helper;
+template<typename ...VariantTypes, typename ...Alternatives>
+struct _holds_any_alternative_helper<std::variant<VariantTypes...>, std::variant<Alternatives...>>{
+    using Variant = std::variant<VariantTypes...>;
+
+    static bool _holds(Variant& var){
+        static_assert(all_in<Collection<Alternatives...>, Collection<VariantTypes...>>::value,
+                      "Can't check if a variant holds an alternative if that alternative is not one of the alternatives defined for the variant, or if the variant is defined to have multiple alternatives of the same type.");
+
+        return (holds_alternative<Alternatives>(var) | ...);
+    }
+};
+
+template<typename ...Alternatives, typename Variant>
+bool holds_any_alternative(Variant& var){
+    return _holds_any_alternative_helper<Variant, variant_join<Alternatives...>>::_holds(var);
+}
+#endif
