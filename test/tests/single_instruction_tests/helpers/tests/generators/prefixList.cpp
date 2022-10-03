f4f9a5d27ef4a815150120290fe8845db4dc75c8
Jory Vardas <joryvardas@gmail.com>
2022-10-03 13:01:50 -0600

    add(test-helpers-generators): prefix list generator
    Add a generator which given a set of prefixes will construct every
    valid subset of prefixes.

---
+++ test/tests/single_instruction_tests/helpers/tests/generators/prefixList.cpp
@@ -0,0 +1,54 @@
+#include "../../generators/prefixList.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+
+using namespace Testing::Helpers::Generators;
+
+TEST_CASE("prefixlistGenerator", "[helper test][generator]") {
+    using PrefixList_t = std::vector<Testing::Helpers::prefix_t>;
+    SECTION("one prefix") {
+        auto gen = generatePrefixList(LOCK_PREFIX);
+        REQUIRE(gen.get().size() == 1);
+        REQUIRE(gen.get() == PrefixList_t{LOCK_PREFIX});
+        REQUIRE(!gen.next());
+    }
+    SECTION("three prefixes from different groups, including a REX prefix") {
+        auto gen = generatePrefixList(LOCK_PREFIX, OPERAND_SIZE_OVERRIDE_PREFIX,
+                                      REX_PREFIX);
+        REQUIRE(gen.get() == PrefixList_t{LOCK_PREFIX,
+                                          OPERAND_SIZE_OVERRIDE_PREFIX,
+                                          REX_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() == PrefixList_t{OPERAND_SIZE_OVERRIDE_PREFIX,
+                                          LOCK_PREFIX, REX_PREFIX});
+        REQUIRE(!gen.next());
+    }
+    SECTION("four prefixes from two different groups") {
+        auto gen = generatePrefixList(LOCK_PREFIX, SS_SEGMENT_OVERRIDE_PREFIX,
+                                      ES_SEGMENT_OVERRIDE_PREFIX, REP_PREFIX);
+        REQUIRE(gen.get() ==
+                PrefixList_t{LOCK_PREFIX, SS_SEGMENT_OVERRIDE_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{SS_SEGMENT_OVERRIDE_PREFIX, LOCK_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{LOCK_PREFIX, ES_SEGMENT_OVERRIDE_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{ES_SEGMENT_OVERRIDE_PREFIX, LOCK_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{REP_PREFIX, SS_SEGMENT_OVERRIDE_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{SS_SEGMENT_OVERRIDE_PREFIX, REP_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{REP_PREFIX, ES_SEGMENT_OVERRIDE_PREFIX});
+        REQUIRE(gen.next());
+        REQUIRE(gen.get() ==
+                PrefixList_t{ES_SEGMENT_OVERRIDE_PREFIX, REP_PREFIX});
+        REQUIRE(!gen.next());
+    }
+}
